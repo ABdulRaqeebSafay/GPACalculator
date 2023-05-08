d4150c7ea2bf11b2ade90c2eaf8bdcace7f1db14
ABdulRaqeebSafay <abdulraqeebsafay@gmail.com>
2023-05-08 16:44:25 +0430

    Calculation of GPA function added

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ function addInput() {
   `;
   inputFields.innerHTML += newInput;
 }
+function calculateGPA() {
+    let totalCredits = 0;
+    let totalGpaTimesCredits = 0;
+    let totalMarks = 0;
+    const marksInputs = document.getElementsByClassName('marksInput');
+    const creditsInputs = document.getElementsByClassName('creditsInput');
+  
+    for (let i = 0; i < numOfSubjects; i++) {
+      const marks = parseFloat(marksInputs[i].value);
+      const credits = parseFloat(creditsInputs[i].value);
+      if (isNaN(marks) || isNaN(credits) || marks < 0 || marks > 100) {
+        alert('Please enter valid marks and credits');
+        return;
+      }
+      const gpa = calculateGpaFromMarks(marks);
+      totalCredits += credits;
+      totalGpaTimesCredits += gpa * credits;
+      totalMarks += marks;
+    }}
